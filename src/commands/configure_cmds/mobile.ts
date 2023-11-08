b40b47b0937340e659f6a906623add7659ac0086
WasiqB <wasbhamla2005@gmail.com>
2023-11-08 13:18:47 +0530

    feat: :sparkles: added name option to web, api and mobile command

--- src/commands/configure_cmds/mobile.ts
+++ src/commands/configure_cmds/mobile.ts
@@ -1,12 +1,33 @@
 import { CommandModule } from "yargs";
 
 export = {
-  command: "mobile",
+  command: "mobile [name]",
   aliases: ["m"],
   describe: "Add Mobile Boyka-Framework Config file",
-  builder: {},
+  builder: (yargs) =>
+    yargs
+      .positional("name", {
+        demandOption: true,
+        describe: "Name of the Mobile config block",
+        type: "string",
+      })
+      .check((argv, options) => {
+        if (!argv.name) {
+          throw new Error("Mobile config name should be provided!");
+        } else {
+          return true;
+        }
+      })
+      .help("help")
+      .showHelpOnFail(
+        true,
+        "whoops, something went wrong in Mobile config! run with help"
+      )
+      .epilog(
+        "For more information, visit: https://boykaframework.github.io/boyka-framework"
+      ),
   handler: (argv) => {
     // TODO: Mobile handler
-    console.log("Handle Mobile Config...");
+    console.log(`Handle Mobile Config ${argv.name}...`);
   },
 } satisfies CommandModule;
--- src/commands/configure_cmds/web.ts
+++ src/commands/configure_cmds/web.ts
@@ -1,12 +1,33 @@
 import { CommandModule } from "yargs";
 
 export = {
-  command: "web",
+  command: "web [name]",
   aliases: ["w"],
   describe: "Add Web Boyka-Framework Config file",
-  builder: {},
+  builder: (yargs) =>
+    yargs
+      .positional("name", {
+        demandOption: true,
+        describe: "Name of the Web config block",
+        type: "string",
+      })
+      .check((argv, options) => {
+        if (!argv.name) {
+          throw new Error("Web config name should be provided!");
+        } else {
+          return true;
+        }
+      })
+      .help("help")
+      .showHelpOnFail(
+        true,
+        "whoops, something went wrong in Web config! run with help"
+      )
+      .epilog(
+        "For more information, visit: https://boykaframework.github.io/boyka-framework"
+      ),
   handler: (argv) => {
     // TODO: web handler
-    console.log("Handle Web Config...");
+    console.log(`Handle Web Config ${argv.name}...`);
   },
 } satisfies CommandModule;

bb2f47c5e051dd420c3f664119da37118d788598
WasiqB <wasbhamla2005@gmail.com>
2023-11-09 12:02:11 +0530

    feat: :sparkles: added eslint and prettier support

--- src/commands/configure_cmds/init.ts
+++ src/commands/configure_cmds/init.ts
@@ -1,12 +1,12 @@
-import { CommandModule } from "yargs";
+import { CommandModule } from 'yargs';
 
 export = {
-  command: "init",
-  aliases: ["i"],
-  describe: "Initialize Boyka-Framework Config file",
+  command: 'init',
+  aliases: ['i'],
+  describe: 'Initialize Boyka-Framework Config file',
   builder: {},
-  handler: (argv) => {
+  handler: () => {
     // TODO: init handler
-    console.log("Handle init Config...");
+    console.log('Handle init Config...');
   },
 } satisfies CommandModule;

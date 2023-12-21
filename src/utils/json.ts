5c1e8ba53c014100d6c8b6514a8aa3a4043167f6
WasiqB <wasbhamla2005@gmail.com>
2023-12-21 11:39:57 +0530

    feat: :lipstick: added message output highlighting

--- src/utils/json.ts
+++ src/utils/json.ts
@@ -1,16 +1,16 @@
 import fs from 'fs';
 import path from 'path';
 import { FrameworkSetting } from '../types/configType';
-import { configFileName } from './constants';
+import { configFileName, errorMessage, successMessage } from './constants';
 
 export const createConfigFile = (filePath: string, setting: FrameworkSetting) => {
   const content = JSON.stringify(setting, null, 2);
   fs.writeFile(path.join(filePath, configFileName), content, (err) => {
     if (err) {
-      console.error(`❌ Error occurred! ${err.message}`);
+      console.error(errorMessage(err));
       process.exit(1);
     } else {
-      console.info(`✅ Boyka config file created at [${filePath}]`);
+      console.info(successMessage(filePath));
     }
   });
 };
